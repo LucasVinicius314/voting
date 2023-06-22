9222da42a325b06ab81fdb24b85027d4f27b9b15
Bruno Rodrigues Faria <80405594+brunofaria27@users.noreply.github.com>
2023-06-22 19:29:32 -0300

    API endpoints OK

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,7 +3,7 @@ import * as cors from 'cors'
 
 import { Request } from 'express'
 
-import { createVote, getVotesPerCandidate } from './services/vote'
+import { createVote, getVotes } from './services/vote'
 import { createParty, getCandidatesWithParty } from './services/party'
 import { createCandidate } from './services/candidate'
 
@@ -13,13 +13,11 @@ const port = 8000
 app.use(express.json()) 
 app.use(cors<Request>())
 
-app.post('/voting', createVote)
-app.get('/analytics', getVotesPerCandidate)
-
-app.post('/party-create', createParty)
-app.get('/candidates', getCandidatesWithParty)
-
-app.post('/candidate-create', createCandidate)
+app.post('/api/voting', createVote)
+app.get('/api/results', getVotes)
+app.post('/api/party', createParty)
+app.get('/api/candidates', getCandidatesWithParty)
+app.post('/api/candidate', createCandidate)
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
--- server/src/services/vote.ts
+++ server/src/services/vote.ts
@@ -51,7 +51,71 @@ export async function createVote(req: Request, res: Response) {
   }
 }
 
-export async function getVotesPerCandidate(req: Request, res: Response) {
- 
+export async function getVotes(req: Request, res: Response) {
+  const client = await connectToDatabase();
+
+  try {
+    const voteCollection = collections.vote;
+    const candidateCollection = collections.candidate;
+    const partyCollection = collections.party;
+
+    if (!voteCollection || !candidateCollection || !partyCollection) {
+      return res.status(500).send(createMessage('As coleções vote, candidate ou party não foram encontradas.'));
+    }
+
+    const candidates = await candidateCollection.find().toArray();
+
+    const presidentCandidatePromises = candidates
+      .filter(candidate => candidate.role === 'president')
+      .map(async (candidate) => {
+        const partyId = candidate.partyId;
+        const party = await partyCollection.findOne({ _id: partyId });
+
+        const presidentVotes = await voteCollection.countDocuments({ presidentId: candidate._id });
+
+        return {
+          primaryLabel: candidate.name,
+          secondaryLabel: party?.name || '',
+          value: presidentVotes,
+        };
+      });
+
+    const mayorCandidatePromises = candidates
+      .filter(candidate => candidate.role === 'mayor')
+      .map(async (candidate) => {
+        const partyId = candidate.partyId;
+        const party = await partyCollection.findOne({ _id: partyId });
+
+        const mayorVotes = await voteCollection.countDocuments({ mayorId: candidate._id });
+
+        return {
+          primaryLabel: candidate.name,
+          secondaryLabel: party?.name || '',
+          value: mayorVotes,
+        };
+      });
+
+    const presidentCandidateVotes = await Promise.all(presidentCandidatePromises);
+    const mayorCandidateVotes = await Promise.all(mayorCandidatePromises);
+
+    const presidentVoteCount = presidentCandidateVotes.reduce((total, candidate) => total + candidate.value, 0);
+    const mayorVoteCount = mayorCandidateVotes.reduce((total, candidate) => total + candidate.value, 0);
+
+    res.json({
+      presidentVoteResults: {
+        voteCount: presidentVoteCount,
+        votesByCandidate: presidentCandidateVotes,
+      },
+      mayorVoteResults: {
+        voteCount: mayorVoteCount,
+        votesByCandidate: mayorCandidateVotes,
+      },
+    });
+  } catch (error) {
+    res.status(500).send(createMessage('Erro ao buscar votos por candidato no banco de dados.'));
+  } finally {
+    if (client) {
+      await closeDatabaseConnection(client);
+    }
+  }
 }
-  
